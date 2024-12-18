36083961bc94b026b7611c87217371b8b874f70f
masun <masunkpck@gmail.com>
2024-12-18 22:26:59 +0300

    components edit

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import Styles from "./ImageCard.module.css";
+import PropTypes from "prop-types";
+
+const ImagePart = ({ image }) => {
+  return (
+    <div className={Styles.imagePart}>
+      <img
+        src={image.urls.small}
+        alt={image.alt_description || "unsplash image"}
+      ></img>
+    </div>
+  );
+};
+
+ImagePart.propTypes = {
+  image: PropTypes.shape({
+    urls: PropTypes.shape({
+      small: PropTypes.string.isRequired, // Use the specific URL property you need
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ImagePart;
